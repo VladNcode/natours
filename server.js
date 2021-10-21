7d657ed4175540cd095bd0430acfc5f87d581bfd
VladNcode <vldsmrfow@gmail.com>
2021-10-21 12:57:25 +0300

    configured eslint and prettier

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(app.get('env'));
-// console.log(process.env);
-
+// console.log(app.get('env'))
+// console.log(process.env)
 //* 4) Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, (req, res) => {
