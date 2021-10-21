65cf4f9220354867f8e2d2a687c021d7f28a98c5
VladNcode <vldsmrfow@gmail.com>
2021-10-21 12:06:31 +0300

    added input checks

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,6 +13,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
+
 //* 2) Route handlers
 exports.getAllTours = (req, res) => {
   res.status(200).json({
@@ -38,7 +48,7 @@ exports.createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
     res.status(201).json({
       status: 'success',
       data: {
@@ -60,6 +70,7 @@ exports.changeTour = (req, res) => {
     },
   });
 };
+
 exports.deleteTour = (req, res) => {
   tours.splice(req.params.id - 1, 1);
   fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {});
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,7 +10,11 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
