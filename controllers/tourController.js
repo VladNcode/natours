b586092580240bba5ce53a5970ede16aec3ce965
VladNcode <vldsmrfow@gmail.com>
2021-10-22 14:37:01 +0300

    refactored code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,7 +5,7 @@ exports.getAllTours = async (req, res) => {
     const allTours = await Tour.find();
     res.status(200).json({
       status: 'success',
-      results: allTours.length,
+      count: allTours.length,
       data: {
         tours: allTours,
       },
@@ -35,30 +35,30 @@ exports.getTour = async (req, res) => {
 };
 exports.createTour = async (req, res) => {
   try {
-    const newTour = await Tour.create(req.body);
+    const tour = await Tour.create(req.body);
     res.status(201).json({
       status: 'success',
       data: {
-        tour: newTour,
+        tour,
       },
     });
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: 'Invalid data sent',
     });
   }
 };
 exports.changeTour = async (req, res) => {
   try {
-    const changeTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
     res.status(200).json({
       status: 'success',
       data: {
-        tour: changeTour,
+        tour,
       },
     });
   } catch (err) {
@@ -70,13 +70,10 @@ exports.changeTour = async (req, res) => {
 };
 exports.deleteTour = async (req, res) => {
   try {
-    const deleteTour = await Tour.findByIdAndDelete(req.params.id);
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
-      message: 'Tour deleted successfully',
-      data: {
-        tour: deleteTour,
-      },
+      data: null,
     });
   } catch (err) {
     res.status(404).json({
