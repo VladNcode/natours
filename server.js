8a1ad728387e20bb3eaa5aa206eb906602d39d82
VladNcode <vldsmrfow@gmail.com>
2021-10-22 11:02:12 +0300

    started using Atlas

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    // console.log(con.connections);
+    console.log('DB connection established');
+  });
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Tour must have a name.'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour must have a price.'],
+  },
+});
+
+const Tour = mongoose.model('Tour', toursSchema);
 
 // console.log(app.get('env'))
 // console.log(process.env)
