5a38afb81eb255a35a8990584c203802d4b17e55
VladNcode <vldsmrfow@gmail.com>
2021-10-27 10:10:45 +0300

    added password encryption

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 12;
 
 const usersSchema = new mongoose.Schema({
   name: {
@@ -24,9 +27,36 @@ const usersSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password.'],
-    maxlength: [8, 'A password must be at least 8 characters'],
+    validate: {
+      // This only works on CREATE AND SAVE!!!
+      validator: function (val) {
+        return val === this.password; // if false = validation error
+      },
+      message: "Passwords doesn't match",
+    },
   },
 });
+
+// Password encryption
+// usersSchema.pre('save', function (next) {
+//   if (!this.isModified('password')) return next();
+// });
+
+usersSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    this.passwordConfirm = undefined;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+usersSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
 
 const User = mongoose.model('User', usersSchema);
 
