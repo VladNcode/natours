8da15fab1e0e065ba52c68411fdd81c0363d6f89
VladNCode <vldsmrfow@gmail.com>
2021-10-27 15:45:51 -0700

    test

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const app = require('./app');
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
+// hi
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
