7f61a92252c81b4c0979bc308f6651255d0423ca
VladNcode <vldsmrfow@gmail.com>
2021-10-27 10:28:30 +0300

    added jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 // const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-  //   if (!newUser) {
-  //     return next(new AppError('No user found with that ID', 404));
-  //   }
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
-  res.status(200).json({
+  res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
