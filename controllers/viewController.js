03b32ca9f674e0cce2d3ff16cf7f6fd0732aa704
VladNCode <vldsmrfow@gmail.com>
2021-11-03 09:13:01 +0200

    filled the overview page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,11 +1,18 @@
-// const catchAsync = require('../utils/catchAsync');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 // const AppError = require('../utils/appError');
 
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from Collection
+  const tours = await Tour.find();
+  // 2) Build template
+  // 3) Render that template using the tour data from 1)
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
