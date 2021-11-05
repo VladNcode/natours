9e9e15209080d5d732dc07b9ef940d07ae783796
VladNCode <vldsmrfow@gmail.com>
2021-11-05 11:15:03 +0200

    added better error handling and error page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,7 @@
 const Tour = require('../models/tourModel');
 // const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from Collection
@@ -18,6 +19,11 @@ exports.getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user',
   });
+
+  if (!tour) {
+    return next(new AppError('No tour found with that name!', 404));
+  }
+
   res.status(200).render('tour', {
     title: tour.name,
     tour,
