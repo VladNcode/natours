68ccf147acc92a7aa9c1261b00f0502bfd225faa
VladNCode <vldsmrfow@gmail.com>
2021-11-06 10:58:21 +0200

    added tour images upload func

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,78 @@
+const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
 // const APIFeatures = require('../utils/apiFeatures');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    return cb(null, true);
+  }
+  cb(new AppError('Not an image, please upload only images!', 400), false);
+};
+
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.uploadTourImages = upload.fields([
+  { name: 'imageCover', maxCount: 1 },
+  { name: 'images', maxCount: 3 },
+]);
+
+// upload.single('image')
+// upload.array('images', 5)
+
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) {
+    return next();
+  }
+
+  // Delete current files before uploading new ones
+  // const tour = await Tour.findById(req.params.id);
+  // if (
+  //   tour.images.length > 0 &&
+  //   fs.existsSync(`public/img/tours/${tour.images[0]}`)
+  // ) {
+  //   tour.images.forEach(image => {
+  //     fs.unlinkSync(`public/img/tours/${image}`);
+  //   });
+  // }
+  // if (tour.imageCover && fs.existsSync(`public/img/tours/${tour.imageCover}`)) {
+  //   fs.unlinkSync(`public/img/tours/${tour.imageCover}`);
+  // }
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    })
+  );
+
+  // console.log(req.body);
+  next();
+});
 
 //! Middleware to prefill top 5
 exports.aliasTopTours = (req, res, next) => {
