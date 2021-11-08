a469af916c5a33652a6f2536f2773ac77d228003
VladNCode <vldsmrfow@gmail.com>
2021-11-08 14:21:04 +0200

    test console.logs

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -30,7 +30,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
         },
       },
@@ -60,6 +64,9 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
+  console.log(tour);
+  console.log(user);
+  console.log(price);
   await Booking.create({ tour, user, price });
 };
 
