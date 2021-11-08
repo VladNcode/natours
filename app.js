60a8c9765830563b57f221db90721c6f2f52ea1e
VladNCode <vldsmrfow@gmail.com>
2021-11-08 14:03:02 +0200

    Improved stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -108,6 +109,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this ip, please try again in an hour!',
 });
 app.use('/api/', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 //* Body parser, reading from body into req.body
 app.use(express.json({ limit: '10kb' }));
