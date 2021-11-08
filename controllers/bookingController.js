a5b836e3e7255a8815888a37ba0cc9373724af4b
VladNCode <vldsmrfow@gmail.com>
2021-11-08 14:34:49 +0200

    final push?

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -61,15 +61,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // };
 
 const createBookingCheckout = async session => {
-  const tour = session.data.object.client_reference_id;
-  const user = (
-    await User.findOne({ email: session.data.object.customer_email })
-  ).id;
-  const price = session.data.object.amount_total / 100;
-
-  console.log(tour);
-  console.log(user);
-  console.log(price);
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
 };
